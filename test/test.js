1558892d16540a871262edb3c5f347f330ecc40d
Boško Bezk <45120471+boskobezik@users.noreply.github.com>
2020-07-11 18:49:07 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,13 +1,8 @@
-// Simple async test for HTTP 200 response code using supertest
-'use strict';
+const describe = require('describe');
 
-var request = require("supertest"),
-    app = require("../app").getApp;
-
-describe('GET /', function(){
-  it('expects HTTP response 200', function(done){
-    request(app)
-     .get('/')
-	 .expect(200, done); 
-  });
+describe("assertions", {
+    'basic synchronous expectation': function() {
+        this.expect(42, 42);
+    }
 });
+
